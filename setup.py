d10789688b5a9cb7587439354d91165eeb668cac
Tom Goetz <tomcgoetz@icloud.com>
2021-06-28 17:37:47 -0400

    single manifest file

--- setup.py
+++ setup.py
@@ -20,17 +20,25 @@ def get_long_description(readme_file):
         return file.read()
 
 
+def get_requirements(requirements_file):
+    """Extract long requirements fron the module requirements.txt."""
+    print(f"Loading requirements from {requirements_file} in {os.getcwd()}")
+    with open(requirements_file, "r", encoding="utf-8") as file:
+        return file.readlines()
+
+
 module_name = 'idbutils'
 module_version = get_version(module_name + os.sep + 'version_info.py')
 module_long_description = get_long_description('README.md')
+install_requires = get_requirements('requirements.txt')
 
-print(f"Building {module_name} {module_version}\n{module_long_description}")
+print(f"Building {module_name} {module_version}")
 
 setup(name=module_name, version=module_version, author='Tom Goetz', packages=[module_name],
       description='Utility library for writing database and internet apps.',
       long_description=module_long_description,
       long_description_content_type='text/markdown',
-      install_requires=open('requirements.txt').readlines(),
+      install_requires=install_requires,
       url="https://github.com/tcgoetz/Fit",
       classifiers=[
           'License :: OSI Approved :: GNU General Public License v2 (GPLv2)',
